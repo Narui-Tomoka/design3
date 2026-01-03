83aed30305ef607cf470cf3b7a8bab24b3cf006b
Tomoka Narui <sweetmelty2512@gmail.com>
2026-01-03 09:13:36 +0900

    実装に不要なコードを削除

--- src/js/utils.js
+++ src/js/utils.js
@@ -17,41 +17,6 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 });
 
-// SAGA MARIAGEの文字を下からスライドして表示する
-// （"text"のclassを持っているspanタグに"move"クラスを追加することでcssアニメーションを起こす）
-const subTitElement = document.querySelector(".fv__subtitle");
-const titElement = document.querySelector(".fv__title");
-
-if (subTitElement) {
-  const observer = new MutationObserver((mutations) => {
-    mutations.forEach((mutation) => {
-      if (
-        mutation.type === "attributes" &&
-        mutation.attributeName === "class"
-      ) {
-        if (subTitElement.classList.contains("move")) {
-          setTimeout(() => {
-            if (titElement) {
-              titElement.classList.add("move");
-            }
-          }, 1000);
-        }
-      }
-    });
-  });
-  // オブザーバーの設定
-  observer.observe(subTitElement, {
-    attributes: true, // class変更を監視する
-  });
-  // ロード時に、observerが動く条件である"move"のクラスをsubTitElementにつける
-  // （オブザーバーの設定後に書かないと動かないので注意！）
-  // loadは画像やフォント含めて読み込み終わってから実行するので今回の用途に適していると考えました
-  // （DOMContentLoadedはHTML読み込みができたらすぐ）
-  window.addEventListener("load", () => {
-    subTitElement.classList.add("move");
-  });
-}
-
 // 要素をフェードインさせるためのjQuery（プラグイン「jquery.inview」使用）
 $(document).ready(function () {
   $(".inview").on("inview", function () {
